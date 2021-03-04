0ca086520a664f6750befee663e346559db3d239
Alexander Chiryatev <a.chiryatev@movavi.com>
2021-03-04 23:59:51 +0700

    add sqlite и sqlite3pp

--- src/ext/sqlite.cmake
+++ src/ext/sqlite.cmake
@@ -1,6 +1,14 @@
 AddTarget(
 	NAME sqlite
 	TYPE static_lib
-	SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/sqlite"
+	SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/sqlite/sqlite"
 	PROJECT_GROUP Util/SQL
+)
+AddTarget(
+	NAME sqlite3pp
+	TYPE static_lib
+	SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/sqlite/sqlite3pp/src"
+	PROJECT_GROUP Util/SQL
+	INCLUDE_DIRS
+		"${CMAKE_CURRENT_LIST_DIR}/sqlite/sqlite"
 )
--- src/home/inpx.cmake
+++ src/home/inpx.cmake
@@ -3,13 +3,16 @@ AddTarget(
 	TYPE app_console
 	SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/inpx"
 	INCLUDE_DIRS
-		"${CMAKE_CURRENT_LIST_DIR}/../ext"
+		"${CMAKE_CURRENT_LIST_DIR}/../ext/sqlite/sqlite"
+		"${CMAKE_CURRENT_LIST_DIR}/../ext/sqlite/sqlite3pp/src"
 		"${CMAKE_CURRENT_LIST_DIR}/../ext/ziplib/Source"
 	LINK_TARGETS
 		bzip2
 		lzma
-		zlib
+		sqlite
+		sqlite3pp
 		ziplib
+		zlib
 	COMPILER_OPTIONS
 		[ MSVC /WX /Wall ]
 )
