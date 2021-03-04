81694fd1d6aa2ae1f0bce6c5703aaef7afdaec65
Alexander Chiryatev <a.chiryatev@movavi.com>
2021-03-04 21:16:26 +0700

    Util/Zip

--- src/ext/ziplib.cmake
+++ src/ext/ziplib.cmake
@@ -2,25 +2,25 @@ AddTarget(
 	NAME bzip2
 	TYPE static_lib
 	SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/ziplib/Source/ZipLib/extlibs/bzip2"
-	PROJECT_GROUP zip
+	PROJECT_GROUP Util/Zip
 )
 AddTarget(
 	NAME lzma
 	TYPE static_lib
 	SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/ziplib/Source/ZipLib/extlibs/lzma"
-	PROJECT_GROUP zip
+	PROJECT_GROUP Util/Zip
 )
 AddTarget(
 	NAME zlib
 	TYPE static_lib
 	SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/ziplib/Source/ZipLib/extlibs/zlib"
-	PROJECT_GROUP zip
+	PROJECT_GROUP Util/Zip
 )
 AddTarget(
 	NAME ziplib
 	TYPE static_lib
 	SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/ziplib/Source/ZipLib"
-	PROJECT_GROUP zip
+	PROJECT_GROUP Util/Zip
 	EXCLUDE_SOURCES "${CMAKE_CURRENT_LIST_DIR}/ziplib/Source/ZipLib/extlibs/"
 	EXPORT_INCLUDE
 )
