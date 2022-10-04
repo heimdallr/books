73ce90a1928971f04ef5cd7c64378cbec6a4f528
Alexander Chiryatev <a.chiryatev@movavi.com>
2022-10-04 13:02:20 +0700

    борьба с модулями

--- src/ext/fmt.cmake
+++ src/ext/fmt.cmake
@@ -2,6 +2,7 @@ AddTarget(
 	NAME fmt
 	TYPE static_lib
 	SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/fmt/src"
+	EXCLUDE_SOURCES "fmt.cc"
 	PROJECT_GROUP Util
 	INCLUDE_DIRS
 		"${CMAKE_CURRENT_LIST_DIR}/fmt/include"
