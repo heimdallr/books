9ee7b887ff322705c33b7b2d817a7cf0826f6e15
Alexander Chiryatev <a.chiryatev@movavi.com>
2022-10-14 01:55:06 +0700

    отдаём наружу модель авторов

--- src/home/flibrary/src/controllers/GuiController.cpp
+++ src/home/flibrary/src/controllers/GuiController.cpp
@@ -1,4 +1,15 @@
+//#include <QAbstractItemModel>
+#include <QQmlEngine>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+#include "fnd/memory.h"
+
+#include "database/factory/Factory.h"
+
+#include "database/interface/Database.h"
+
+#include "models/AuthorsModel.h"
 
 #include "GuiController.h"
 
@@ -7,9 +18,11 @@ namespace HomeCompa::Flibrary {
 class GuiController::Impl
 {
 public:
-	Impl()
+	explicit Impl(const std::string & databaseName)
+		: m_db(Create(DB::Factory::Impl::Sqlite, databaseName))
+		, m_authorsModel(std::unique_ptr<QAbstractItemModel>(CreateAuthorsModel(*m_db)))
 	{
-		m_qmlEngine.load("qrc:/qml/Main.qml");
+		QQmlEngine::setObjectOwnership(m_authorsModel.get(), QQmlEngine::CppOwnership);
 	}
 
 	~Impl()
@@ -17,11 +30,41 @@ public:
 		m_qmlEngine.clearComponentCache();
 	}
 
-private:
+	void Start(GuiController * guiController)
+	{
+		auto * const qmlContext = m_qmlEngine.rootContext();
+		qmlContext->setContextProperty("guiController", guiController);
+		qRegisterMetaType<QAbstractItemModel *>("QAbstractItemModel*");
+		m_qmlEngine.load("qrc:/qml/Main.qml");
+	}
+	
+	QAbstractItemModel * GetAuthorsModel()
+	{
+		return m_authorsModel.get();
+	}
+
+private:	
 	QQmlApplicationEngine m_qmlEngine;
+	PropagateConstPtr<DB::Database> m_db;
+	PropagateConstPtr<QAbstractItemModel> m_authorsModel;
 };
 
-GuiController::GuiController() = default;
+GuiController::GuiController(const std::string & databaseName, QObject * parent)
+	: QObject(parent)
+	, m_impl(databaseName)
+{
+}
+
 GuiController::~GuiController() = default;
 
+QAbstractItemModel * GuiController::GetAuthorsModel()
+{
+	return m_impl->GetAuthorsModel();
+}
+
+void GuiController::Start()
+{
+	m_impl->Start(this);
+}
+	
 }
--- src/home/flibrary/src/controllers/GuiController.h
+++ src/home/flibrary/src/controllers/GuiController.h
@@ -1,14 +1,27 @@
 #pragma once
 
+#include <string>
+
+#include <QObject>
+
 #include "fnd/memory.h"
+
+class QAbstractItemModel;
 
 namespace HomeCompa::Flibrary {
 
 class GuiController
+	: public QObject
 {
+	Q_OBJECT
 public:
-	GuiController();
-	~GuiController();
+	explicit GuiController(const std::string & databaseName, QObject * parent = nullptr);
+	~GuiController() override;
+
+	void Start();
+
+public:
+	Q_INVOKABLE QAbstractItemModel * GetAuthorsModel();
 
 private:
 	class Impl;
