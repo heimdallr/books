ac0814aaad9ef7cdaab0beddd7d4e1317a0f47f5
Alexander Chiryatev <a.chiryatev@movavi.com>
2022-10-14 18:01:21 +0700

    Set переехал в общий код

---
+++ src/home/fnd/algorithm.h
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace HomeCompa::Util {
+
+template
+	< typename T
+	, typename Obj
+	, typename Signal = void(Obj:: *)() const
+	>
+static bool Set(T & dst, const T value, const Obj & obj, const Signal signal)
+{
+	if (dst == value)
+		return false;
+
+	dst = value;
+	emit (obj.*signal)();
+	return true;
+}
+
+}
