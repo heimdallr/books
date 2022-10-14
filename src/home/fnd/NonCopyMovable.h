00c44a8a10dec0c23472bb3bfaff593621220199
Alexander Chiryatev <a.chiryatev@movavi.com>
2022-10-14 18:01:54 +0700

    макрос NON_COPY_MOVABLE

---
+++ src/home/fnd/NonCopyMovable.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define NON_COPYABLE(NAME)                    \
+public:                                       \
+NAME(const NAME &) = delete;                  \
+NAME & operator=(const NAME &) = delete;      \
+private:
+
+#define NON_MOVABLE(NAME)                     \
+public:                                       \
+NAME(NAME &&) noexcept = delete;              \
+NAME & operator=(NAME &&) noexcept = delete;  \
+private:
+
+// rule 5
+#define NON_COPY_MOVABLE(NAME) NON_COPYABLE(NAME) NON_MOVABLE(NAME)
