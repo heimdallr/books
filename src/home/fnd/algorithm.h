81e0f6f08bfa65f925309468e8c5bf16980959e8
Alexander Chiryatev <a.chiryatev@movavi.com>
2022-10-15 21:42:29 +0700

    Set для non-const

--- src/home/fnd/algorithm.h
+++ src/home/fnd/algorithm.h
@@ -5,9 +5,24 @@ namespace HomeCompa::Util {
 template
 	< typename T
 	, typename Obj
+	, typename Signal = void(Obj:: *)()
+	>
+static bool Set(T & dst, const T & value, Obj & obj, const Signal signal)
+{
+	if (dst == value)
+		return false;
+
+	dst = value;
+	emit (obj.*signal)();
+	return true;
+}
+
+template
+	< typename T
+	, typename Obj
 	, typename Signal = void(Obj:: *)() const
 	>
-static bool Set(T & dst, const T value, const Obj & obj, const Signal signal)
+static bool Set(T & dst, const T & value, const Obj & obj, const Signal signal)
 {
 	if (dst == value)
 		return false;
