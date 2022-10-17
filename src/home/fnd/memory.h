54933c0098f1565e96b10a4961e135adbd65c9ee
Alexander Chiryatev <a.chiryatev@movavi.com>
2022-10-17 14:36:39 +0700

    Добавлен конструктор из сырого указателя для PropagateConstPtr

--- src/home/fnd/memory.h
+++ src/home/fnd/memory.h
@@ -9,85 +9,85 @@ namespace HomeCompa {
 template <class T>
 class NotNull
 {
-    static_assert(std::is_assignable<T &, std::nullptr_t>::value, "T cannot be assigned nullptr.");
+	static_assert(std::is_assignable<T &, std::nullptr_t>::value, "T cannot be assigned nullptr.");
 
 public:
-    NotNull(T t)
-        : m_ptr(t)
-    {
-        ensure_invariant();
+	NotNull(T t)
+		: m_ptr(t)
+	{
+		ensure_invariant();
+	}
+
+	template <typename U>
+	NotNull(U t)
+		: m_ptr(t)
+	{
+		ensure_invariant();
+	}
+
+	NotNull(const NotNull & rhs)
+		: m_ptr(rhs.m_ptr)
+	{}
+
+	template <typename U>
+	NotNull & operator=(const U & t)
+	{
+		m_ptr = t;
+		ensure_invariant();
+		return *this;
+	}
+
+	NotNull & operator=(const T & t)
+	{
+		m_ptr = t;
+		ensure_invariant();
+		return *this;
+	}
+
+	NotNull & operator=(const NotNull & rhs)
+	{
+		if (&rhs != this)
+			m_ptr = rhs.m_ptr;
+		return *this;
     }
 
-    template <typename U>
-    NotNull(U t)
-        : m_ptr(t)
-    {
-        ensure_invariant();
-    }
+	template <typename U, typename Dummy = typename std::enable_if<std::is_convertible<U, T>::value>::type>
+	NotNull(const NotNull<U> & rhs)
+	{
+		*this = rhs;
+	}
 
-    NotNull(const NotNull & rhs)
-        : m_ptr(rhs.m_ptr)
-    {}
+	template <typename U, typename Dummy = typename std::enable_if<std::is_convertible<U, T>::value>::type>
+	NotNull & operator=(const NotNull<U> & rhs)
+	{
+		m_ptr = rhs.Get();
+		return *this;
+	}
 
-    template <typename U>
-    NotNull & operator=(const U & t)
-    {
-        m_ptr = t;
-        ensure_invariant();
-        return *this;
-    }
+	T Get() const { return m_ptr; }
 
-    NotNull & operator=(const T & t)
-    {
-        m_ptr = t;
-        ensure_invariant();
-        return *this;
-    }
+	operator T() const { return Get(); }
+	T operator->() const { return Get(); }
 
-    NotNull & operator=(const NotNull & rhs)
-    {
-        if (&rhs != this)
-            m_ptr = rhs.m_ptr;
-        return *this;
-    }
-
-    template <typename U, typename Dummy = typename std::enable_if<std::is_convertible<U, T>::value>::type>
-    NotNull(const NotNull<U> & rhs)
-    {
-        *this = rhs;
-    }
-
-    template <typename U, typename Dummy = typename std::enable_if<std::is_convertible<U, T>::value>::type>
-    NotNull & operator=(const NotNull<U> & rhs)
-    {
-        m_ptr = rhs.Get();
-        return *this;
-    }
-
-    T Get() const { return m_ptr; }
-
-    operator T() const { return Get(); }
-    T operator->() const { return Get(); }
-
-    bool operator==(const T & rhs) const { return m_ptr == rhs; }
-    bool operator!=(const T & rhs) const { return !(*this == rhs); }
+	bool operator==(const T & rhs) const { return m_ptr == rhs; }
+	bool operator!=(const T & rhs) const { return !(*this == rhs); }
 
 private:
-    NotNull(std::nullptr_t); // = delete;
-    NotNull(int); // = delete;
-    NotNull<T> & operator=(std::nullptr_t); // = delete;
-    NotNull<T> & operator=(int); // = delete;
+	NotNull(std::nullptr_t); // = delete;
+	NotNull(int); // = delete;
+	NotNull<T> & operator=(std::nullptr_t); // = delete;
+	NotNull<T> & operator=(int); // = delete;
 
 private:
-    T m_ptr;
-    void ensure_invariant() const
-    {
-        if (!m_ptr)
-        {
-            assert(false);
-            throw(std::logic_error("NotNull can not initialized by null"));
-        }
-    }
+	T m_ptr;
+	void ensure_invariant() const
+	{
+		if (!m_ptr)
+		{
+			assert(false);
+			throw(std::logic_error("NotNull can not initialized by null"));
+		}
+	}
 };
 
 namespace details {
@@ -98,21 +98,21 @@ struct PropogateConstCreator;
 template <class T>
 struct PropogateConstCreator<std::shared_ptr<T>>
 {
-    template <class... ARGS>
-    std::shared_ptr<T> operator()(ARGS &&... args)
-    {
-        return std::make_shared<T>(std::forward<ARGS>(args)...);
-    }
+	template <class... ARGS>
+	std::shared_ptr<T> operator()(ARGS &&... args)
+	{
+		return std::make_shared<T>(std::forward<ARGS>(args)...);
+	}
 };
 
 template <class T>
 struct PropogateConstCreator<std::unique_ptr<T>>
 {
-    template <class... ARGS>
-    std::unique_ptr<T> operator()(ARGS &&... args)
-    {
-        return std::make_unique<T>(std::forward<ARGS>(args)...);
-    }
+	template <class... ARGS>
+	std::unique_ptr<T> operator()(ARGS &&... args)
+	{
+		return std::make_unique<T>(std::forward<ARGS>(args)...);
+	}
 };
 
 } // namespace details
@@ -121,39 +121,44 @@ template <class T, template <class...> class P = std::unique_ptr>
 class PropagateConstPtr
 {
 public:
-    template <class... ARGS>
-    PropagateConstPtr(ARGS &&... args)
-        : m_p(details::PropogateConstCreator<P<T>>()(std::forward<ARGS>(args)...))
-    {}
+	template <class... ARGS>
+	explicit PropagateConstPtr(ARGS &&... args)
+		: m_p(details::PropogateConstCreator<P<T>>()(std::forward<ARGS>(args)...))
+	{}
 
-    explicit PropagateConstPtr(P<T> && p)
-        : m_p(std::move(p))
-    {}
+	explicit PropagateConstPtr(P<T> && p)
+		: m_p(std::move(p))
+	{}
 
-    PropagateConstPtr(PropagateConstPtr &&) noexcept = default;
+	explicit PropagateConstPtr(T * p)
+	: m_p(p)
+	{
+	}
 
-    PropagateConstPtr & operator=(PropagateConstPtr &&) noexcept = default;
+	PropagateConstPtr(PropagateConstPtr &&) noexcept = default;
 
-    ~PropagateConstPtr() = default;
+	PropagateConstPtr & operator=(PropagateConstPtr &&) noexcept = default;
 
-    const T * get() const noexcept { return m_p.get(); }
-    T * get() noexcept { return m_p.get(); }
+	~PropagateConstPtr() = default;
 
-    const T * operator->() const noexcept { return get(); }
-    T * operator->() noexcept { return get(); }
+	const T * get() const noexcept { return m_p.get(); }
+	T * get() noexcept { return m_p.get(); }
 
-    const T & operator*() const noexcept { return *get(); }
-    T & operator*() noexcept { return *get(); }
+	const T * operator->() const noexcept { return get(); }
+	T * operator->() noexcept { return get(); }
 
-    explicit operator const T *() const noexcept { return get(); }
-    explicit operator T *() noexcept { return get(); }
+	const T & operator*() const noexcept { return *get(); }
+	T & operator*() noexcept { return *get(); }
 
-    void swap(PropagateConstPtr & rhs) noexcept { rhs.m_p.swap(m_p); }
-    void reset() { m_p.reset(); }
-    void reset(T * p) noexcept { m_p.reset(p); }
+	explicit operator const T *() const noexcept { return get(); }
+	explicit operator T *() noexcept { return get(); }
+
+	void swap(PropagateConstPtr & rhs) noexcept { rhs.m_p.swap(m_p); }
+	void reset() { m_p.reset(); }
+	void reset(T * p) noexcept { m_p.reset(p); }
 
 private:
-    P<T> m_p;
+	P<T> m_p;
 };
 
 }
