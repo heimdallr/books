f9960b1c2816b7db9d1b115590eb9ee29d3161e0
Alexander Chiryatev <a.chiryatev@movavi.com>
2022-12-07 16:16:54 +0700

    Обёртка для инклюов Poco/Zip

---
+++ src/ext/include/Poco/Zip.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#pragma warning(push)
+#pragma warning(disable: 5054)
+#undef NOMINMAX
+#include <Poco/Zip/ZipArchive.h>
+#include <Poco/Zip/ZipStream.h>
+#include <Poco/Zip/Compress.h>
+#include <Poco/File.h>
+#include <Poco/MemoryStream.h>
+#pragma warning(pop)
