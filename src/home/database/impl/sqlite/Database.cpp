5d110c7d24b41707d974b3797bb18bb56f1da44a
Alexander Chiryatev <a.chiryatev@movavi.com>
2023-01-20 11:56:33 +0700

    Для БД добавлен обсервер

--- src/home/database/impl/sqlite/Database.cpp
+++ src/home/database/impl/sqlite/Database.cpp
@@ -2,6 +2,9 @@
 #include <sstream>
 #include <string>
 
+#include "fnd/FindPair.h"
+#include "fnd/observable.h"
+
 #include "database/interface/Database.h"
 #include "database/interface/Transaction.h"
 #include "database/interface/Query.h"
@@ -21,6 +24,15 @@ constexpr auto PATH = "path";
 constexpr auto EXTENSION = "extension";
 
 using ConnectionParameters = std::multimap<std::string, std::string>;
+
+using ObserverMethod = void(DatabaseObserver:: *)(char const * dbName, char const * tableName, int64_t rowId);
+// ocCodes: sqlite3.cpp
+constexpr std::pair<int, ObserverMethod> g_opCodeToObserverMethod[]
+{
+	{ 18, &DatabaseObserver::OnInsert },
+	{  9, &DatabaseObserver::OnDelete },
+	{ 23, &DatabaseObserver::OnUpdate },
+};
 
 std::vector<std::string> Split(const std::string & src, const char separator)
 {
@@ -59,14 +71,39 @@ const std::string & GetValue(const ConnectionParameters & parameters, const std:
 
 class Database
 	: virtual public DB::Database
+	, public Observable<DatabaseObserver>
 {
+private:
+	struct ObserverImpl
+	{
+		explicit ObserverImpl(Database & self)
+			: m_self(self)
+		{
+		}
+
+		void OnUpdate(int opCode, char const * dbName, char const * tableName, int64_t rowId) const
+		{
+			const auto method = FindSecond(g_opCodeToObserverMethod, opCode);
+			m_self.Perform(method, dbName, tableName, rowId);
+		}
+
+	private:
+		Database & m_self;
+	};
+
 public:
 	explicit Database(const std::string & connection)
 		: m_connectionParameters(ParseConnectionString(connection))
 		, m_db(GetValue(m_connectionParameters, PATH).data(), SQLITE_OPEN_READWRITE)
+		, m_observer(*this)
 	{
 		for (auto [begin, end] = m_connectionParameters.equal_range(EXTENSION); begin != end; ++begin)
 			m_db.load_extension(begin->second.data());
+
+		m_db.set_update_handler([&](int opCode, char const * dbName, char const * tableName, int64_t rowId)
+		{
+			m_observer.OnUpdate(opCode, dbName, tableName, rowId);
+		});
 	}
 
 private: // Database
@@ -80,10 +117,21 @@ private: // Database
 		return CreateQueryImpl(m_guard, m_db, query);
 	}
 
+	void RegisterObserver(DatabaseObserver * observer)
+	{
+		Register(observer);
+	}
+
+	void UnregisterObserver(DatabaseObserver * observer)
+	{
+		UnregisterObserver(observer);
+	}
+
 private:
 	ConnectionParameters m_connectionParameters;
 	std::shared_mutex m_guard;
 	sqlite3pp::database m_db;
+	ObserverImpl m_observer;
 };
 
 }
--- src/home/database/interface/Database.h
+++ src/home/database/interface/Database.h
@@ -3,10 +3,20 @@
 #include <memory>
 #include <string_view>
 
+#include "fnd/observer.h"
+
 namespace HomeCompa::DB {
 
 class Transaction;
 class Query;
+
+class DatabaseObserver : public Observer
+{
+public:
+	virtual void OnInsert(char const * dbName, char const * tableName, int64_t rowId) = 0;
+	virtual void OnUpdate(char const * dbName, char const * tableName, int64_t rowId) = 0;
+	virtual void OnDelete(char const * dbName, char const * tableName, int64_t rowId) = 0;
+};
 
 class Database
 {
@@ -14,6 +24,9 @@ public:
 	virtual ~Database() = default;
 	virtual [[nodiscard]] std::unique_ptr<Transaction> CreateTransaction() = 0;
 	virtual [[nodiscard]] std::unique_ptr<Query> CreateQuery(const std::string_view & query) = 0;
+
+	virtual void RegisterObserver(DatabaseObserver * observer) = 0;
+	virtual void UnregisterObserver(DatabaseObserver * observer) = 0;
 };
 
 }
