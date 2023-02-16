649bbaf418f1fb211deb3194cc2cf36bcd79c15c
Alexander Chiryatev <heimdallr@ngs.ru>
2023-02-16 21:41:33 +0700

    двинули версию студии

--- solution_generate.bat
+++ solution_generate.bat
@@ -1,21 +1,20 @@
 rem @echo off
 
 set start_time=%DATE% %TIME%
-set cmake_path=D:\programs\cmake\3.20.3\bin\cmake.exe
+set cmake_path=D:\programs\cmake\3.26.0\bin\cmake.exe
 
 set tee_name=tee.exe
 call src\ext\scripts\batch\check_executable.bat %tee_name%
 if NOT [%ERRORLEVEL%]==[0] goto end
 
+set GENERATOR="Visual Studio 17 2022"
 set originalDir=%CD%
 if [%WIN64BUILD%]==[true] (
 	set GENERATOR_PLATFORM=x64
 	set PLATFORM="64"
-	set GENERATOR="Visual Studio 16 2019"
 ) else (
 	set GENERATOR_PLATFORM=Win32
 	set PLATFORM=
-	set GENERATOR="Visual Studio 16 2019"
 )
 
 echo | set /p=%PLATFORM%>%TEMP%\platform.ini
