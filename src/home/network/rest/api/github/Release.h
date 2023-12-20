db59a76453d04d4fb3bfe4adb9aef7980b7bb132
Alexander Chiryatev <heimdallrnsk@gmail.com>
2023-12-20 15:56:22 +0700

    #93 GitHub API. Получение информации о последнем релизе

---
+++ src/home/network/rest/api/github/Release.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <QString>
+
+class QJsonValue;
+
+namespace HomeCompa::RestAPI::Github {
+
+class IClient;
+
+struct Asset;
+using Assets = std::vector<Asset>;
+struct Asset
+{
+	int64_t id { -1 };
+	QString name;
+	int64_t size { -1 };
+	int64_t download_count { -1 };
+	QString browser_download_url;
+
+	static Assets ParseAssets(const QJsonValue & data);
+};
+
+struct Release
+{
+	int64_t id { -1 };
+	QString name;
+	QString tag_name;
+	QString html_url;
+	Assets assets;
+
+	static void ParseGetLatestRelease(IClient & client, const QJsonValue & data);
+
+	explicit Release(const QJsonValue & data);
+	Release() = default;
+	~Release() = default;
+	Release(const Release &) = default;
+	Release(Release &&) = default;
+	Release & operator=(const Release &) = default;
+	Release & operator=(Release &&) = default;
+};
+
+}
---
+++ src/home/network/rest/connection/BaseConnection.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <map>
+
+#include "fnd/memory.h"
+#include "fnd/NonCopyMovable.h"
+
+#include "IConnection.h"
+
+class QJsonDocument;
+
+namespace HomeCompa::RestAPI {
+
+class BaseConnection
+	: virtual public IConnection
+{
+	NON_COPY_MOVABLE(BaseConnection)
+
+protected:
+	BaseConnection(std::string address, Headers headers);
+	~BaseConnection() override;
+
+protected: // IConnection
+	void Get(const std::string & request) final;
+	const std::string & Url() const noexcept final;
+	const Headers & GetHeaders() const noexcept final;
+	void Register(IObserver * observer) final;
+	void Unregister(IObserver * observer) final;
+
+private:
+	virtual Headers GetPage(const std::string & request) = 0;
+
+protected:
+	const std::string & GetAddress() const noexcept;
+	void OnDataReceived(const QJsonDocument & json);
+
+private:
+	struct Impl;
+	PropagateConstPtr<Impl> m_impl;
+};
+
+}
