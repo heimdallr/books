10d23b3828d00eb6034c29cf497d7828affa7feb
Alexander Chiryatev <heimdallrnsk@gmail.com>
2023-12-20 19:39:30 +0700

    #93 Простая скачивалка

---
+++ src/home/network/network/downloader.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <functional>
+
+#include "fnd/memory.h"
+#include "fnd/NonCopyMovable.h"
+
+#include "export/network.h"
+
+class QIODevice;
+class QString;
+
+namespace HomeCompa::Network {
+
+class NETWORK_EXPORT Downloader
+{
+	NON_COPY_MOVABLE(Downloader)
+
+public:
+	using OnFinish = std::function<void(int code, const QString & message)>;
+	using OnProgress = std::function<void(int64_t bytesReceived, int64_t bytesTotal)>;
+
+public:
+	Downloader();
+	~Downloader();
+
+public:
+	void Download(const QString & url, QIODevice & io, OnFinish callback, OnProgress progress = {});
+
+private:
+	class Impl;
+	PropagateConstPtr<Impl> m_impl;
+};
+
+}
