6e08422f6c57ca345f0ed8059d74945274561853
Alexander Chiryatev <heimdallrnsk@gmail.com>
2023-12-26 22:00:34 +0700

    #113 Перевод восстановления картинок на xerces-c

--- src/home/flibrary/logic/shared/ImageRestore.cpp
+++ src/home/flibrary/logic/shared/ImageRestore.cpp
@@ -1,12 +1,12 @@
 #include "ImageRestore.h"
 
 #include <QBuffer>
-#include <QDomNode>
 #include <QFileInfo>
 
 #include <plog/Log.h>
 
 #include "util/xml/SaxParser.h"
+#include "util/xml/XmlAttributes.h"
 #include "util/xml/XmlWriter.h"
 
 #include "zip.h"
@@ -20,11 +20,18 @@ class SaxPrinter final
 	: public Util::SaxParser
 {
 public:
-	explicit SaxPrinter(QIODevice & input, QIODevice & output)
+	SaxPrinter(QIODevice & input, QIODevice & output, Zip & zip, const QString & bookFileName)
 		: SaxParser(input)
 		, m_writer(output)
+		, m_zip(zip)
+		, m_bookFileName(bookFileName)
 	{
 		Parse();
+	}
+
+	bool HasError() const noexcept
+	{
+		return m_hasError;
 	}
 
 private: // Util::SaxParser
@@ -35,7 +42,12 @@ private: // Util::SaxParser
 
 	bool OnStartElement(const QString & name, const QString & /*path*/, const Util::XmlAttributes & attributes) override
 	{
-		return m_writer.WriteStartElement(name, attributes), true;
+		m_writer.WriteStartElement(name, attributes);
+
+		if (name == "binary")
+			WriteImage(attributes.GetAttribute("id"));
+
+		return true;
 	}
 
 	bool OnEndElement(const QString & name, const QString & /*path*/) override
@@ -48,26 +60,58 @@ private: // Util::SaxParser
 		return m_writer.WriteCharacters(value), true;
 	}
 
-	bool OnWarning(const QString & /*text*/) override
+	bool OnWarning(const QString & text) override
 	{
+		PLOGW << text;
 		return true;
 	}
 
-	bool OnError(const QString & /*text*/) override
+	bool OnError(const QString & text) override
 	{
-		return true;
+		m_hasError = true;
+		PLOGE << text;
+		return false;
 	}
 
-	bool OnFatalError(const QString & /*text*/) override
+	bool OnFatalError(const QString & text) override
 	{
-		return true;
+		return OnError(text);
+	}
+
+private:
+	void WriteImage(const QString & imageFileName)
+	{
+		const auto data = [&]
+		{
+			try
+			{
+				return m_zip.Read(QString("%1/%2").arg(m_bookFileName, imageFileName)).readAll();
+			}
+			catch (const std::exception & ex)
+			{
+				PLOGE << ex.what();
+			}
+			return QByteArray {};
+		}();
+
+		if (data.isEmpty())
+		{
+			PLOGW << imageFileName << " not found";
+			return;
+		}
+
+		const auto image = QString::fromUtf8(data.toBase64());
+		OnCharacters({}, image);
 	}
 
 private:
 	Util::XmlWriter m_writer;
+	Zip & m_zip;
+	const QString & m_bookFileName;
+	bool m_hasError { false };
 };
 
-QByteArray RestoreImagesImpl(QIODevice & stream, const QString & folder, const QString & /*fileName*/)
+QByteArray RestoreImagesImpl(QIODevice & stream, const QString & folder, const QString & fileName)
 {
 	const auto zip = CreateImageArchive(folder);
 	if (!zip)
@@ -76,55 +120,13 @@ QByteArray RestoreImagesImpl(QIODevice & stream, const QString & folder, const Q
 	QByteArray byteArray;
 	QBuffer buffer(&byteArray);
 	buffer.open(QIODevice::WriteOnly);
-	SaxPrinter saxPrinter(stream, buffer);
+	const SaxPrinter saxPrinter(stream, buffer, *zip, fileName);
 	buffer.close();
-	return byteArray;
+	if (!saxPrinter.HasError())
+		return byteArray;
 
-//	auto input = stream.readAll();
-//	QDomDocument doc;
-//	if (!doc.setContent(input))
-//		return input;
-//
-//	const auto root = doc.documentElement();
-//
-//	std::vector<QDomNode> nodes;
-//
-//	const auto nodeList = root.elementsByTagName("binary");
-//	for (int i = 0, sz = nodeList.size(); i < sz; ++i)
-//		if (auto node = nodeList.item(i); node.nodeValue().isEmpty())
-//			nodes.push_back(std::move(node));
-//
-//	if (nodes.empty())
-//		return input;
-//
-//
-//	const auto read = [&] (const QString & id)
-//	{
-//		try
-//		{
-//			return zip->Read(QString("%1/%2").arg(fileName, id)).readAll();
-//		}
-//		catch (const std::exception & ex)
-//		{
-//			PLOGE << ex.what();
-//		}
-//		return QByteArray {};
-//	};
-//
-//
-//	for (auto & node : nodes)
-//	{
-//		const auto bytes = read(node.attributes().namedItem("id").nodeValue());
-//		if (bytes.isEmpty())
-//			continue;
-//
-//		auto image = QString::fromUtf8(bytes.toBase64());
-//		node.appendChild(doc.createTextNode(image));
-//	}
-//
-//	doc.removeChild(doc.firstChild());
-//	doc.insertBefore(doc.createProcessingInstruction("xml", R"(version="1.0" encoding="utf-8")"), doc.firstChild());
-//	return doc.toByteArray();
+	stream.seek(0);
+	return stream.readAll();
 }
 
 }
