25a35c9d217f04e669399ec62deda29835dcb382
Alexander Chiryatev <heimdallrnsk@gmail.com>
2023-12-28 00:41:57 +0700

    #115 Восстановление пользовательских данных переписано на xerces-c

--- src/home/flibrary/logic/userdata/restore.h
+++ src/home/flibrary/logic/userdata/restore.h
@@ -10,10 +10,19 @@ class IDatabase;
 
 namespace HomeCompa::Util {
 class IExecutor;
+class XmlAttributes;
 }
 
 namespace HomeCompa::Flibrary::UserData {
 
 void Restore(Util::IExecutor & executor, DB::IDatabase & db, QString fileName, Callback callback);
+
+class IRestorer  // NOLINT(cppcoreguidelines-special-member-functions)
+{
+public:
+	virtual ~IRestorer() = default;
+	virtual void AddElement(const QString & name, const Util::XmlAttributes & attributes) = 0;
+	virtual void Restore(DB::IDatabase & db) const = 0;
+};
 
 }
