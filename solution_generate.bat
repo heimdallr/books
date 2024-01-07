f9831131c31a22fe8c01300277a994ca11a03fd0
Alexander Chiryatev <heimdallrnsk@gmail.com>
2024-01-07 20:05:49 +0700

    Версия - вперёд
    [TECH]

--- solution_generate.bat
+++ solution_generate.bat
@@ -1,7 +1,7 @@
 rem @echo off
 
 set start_time=%DATE% %TIME%
-set PRODUCT_VERSION=0.9.8
+set PRODUCT_VERSION=0.9.9
 
 call src\ext\scripts\batch\check_executable.bat cmake
 if NOT [%ERRORLEVEL%]==[0] goto end
