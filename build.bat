db169958663dd2ef0b1b7cd5692f78630d100cf9
Alexander Chiryatev <heimdallrnsk@gmail.com>
2024-02-25 01:53:21 +0700

    #178 Генерим архив с портабельной версией

--- build.bat
+++ build.bat
@@ -17,6 +17,9 @@ rem if %errorlevel% NEQ 0 goto Error
 echo installer creating
 ISCC.exe /DRootDir=%~dp0 /DMyAppVersion=%PRODUCT_VERSION% %~dp0src\home\flibrary\app\resources\installer\flibrary.iss
 
+echo portable creating
+7z a %~dp0build\bin\installer\flibrary_portable_%PRODUCT_VERSION%.7z %~dp0build\bin\%BUILD_TYPE%\*
+
 goto End
 
 :Error
