14b1b25ecf8fe171733cd0f8f024a83ca256a6dd
Alexander Chiryatev <heimdallrnsk@gmail.com>
2024-02-24 23:31:13 +0700

    #178 Долй хардкод, больше констант

--- src/home/flibrary/logic/Collection/CollectionController.cpp
+++ src/home/flibrary/logic/Collection/CollectionController.cpp
@@ -64,9 +64,9 @@ IniMapPair GetIniMap(const QString & db, const QString & folder, bool createFile
 	result.second = Inpx::Parser::IniMap
 	{
 		{ DB_PATH, db.toStdWString() },
-		{ DB_CREATE_SCRIPT, getFile("CreateCollection.sql").toStdWString() },
-		{ DB_UPDATE_SCRIPT, getFile("UpdateCollection.sql").toStdWString() },
 		{ GENRES, getFile(QString::fromStdWString(DEFAULT_GENRES)).toStdWString() },
+		{ DB_CREATE_SCRIPT, getFile(QString::fromStdWString(DEFAULT_DB_CREATE_SCRIPT)).toStdWString() },
+		{ DB_UPDATE_SCRIPT, getFile(QString::fromStdWString(DEFAULT_DB_UPDATE_SCRIPT)).toStdWString() },
 		{ INPX, inpx.toStdWString() },
 	};
 
