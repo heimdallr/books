bc84289b99505f05ecbecea1368a7dbf85bc3f4a
Alexander Chiryatev <heimdallrnsk@gmail.com>
2024-03-29 19:36:40 +0700

    #191 Номера строк в ts-ках

--- src/home/flibrary/gui/resources/locales/en.ts
+++ src/home/flibrary/gui/resources/locales/en.ts
@@ -584,12 +584,12 @@ Restart now?</source>
 <context>
     <name>TreeView</name>
     <message>
-        <location filename="../../TreeView.cpp" line="56"/>
+        <location filename="../../TreeView.cpp" line="58"/>
         <source>Find</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../TreeView.cpp" line="57"/>
+        <location filename="../../TreeView.cpp" line="59"/>
         <source>Filter</source>
         <translation></translation>
     </message>
