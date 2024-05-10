283df83465ef4ef30c67518b8c27923c7a943f4d
Alexander Chiryatev <heimdallrnsk@gmail.com>
2024-05-10 12:48:07 +0700

    #198 ClickableLabel обучен double click'у

--- src/home/flibrary/gui/ClickableLabel.cpp
+++ src/home/flibrary/gui/ClickableLabel.cpp
@@ -1,16 +1,44 @@
 #include "ClickableLabel.h"
 
 #include <QMouseEvent>
+#include <QTimer>
 
 using namespace HomeCompa::Flibrary;
+
+struct ClickableLabel::Impl
+{
+	QTimer timer;
+	QPoint pos;
+
+	Impl()
+	{
+		timer.setSingleShot(true);
+		timer.setInterval(std::chrono::milliseconds(300));
+	}
+};
 
 ClickableLabel::ClickableLabel(QWidget * parent)
 	: QLabel(parent)
 {
+	connect(&m_impl->timer, &QTimer::timeout, [this]
+	{
+		emit clicked(m_impl->pos);
+	});
 }
+
+ClickableLabel::~ClickableLabel() = default;
 
 void ClickableLabel::mousePressEvent(QMouseEvent * event)
 {
 	if (event->button() == Qt::LeftButton)
-		emit clicked(event->pos());
+	{
+		if (m_impl->timer.isActive())
+		{
+			m_impl->timer.stop();
+			return emit doubleClicked(event->pos());
+		}
+
+		m_impl->pos = event->pos();
+		m_impl->timer.start();
+	}
 }
--- src/home/flibrary/gui/ClickableLabel.h
+++ src/home/flibrary/gui/ClickableLabel.h
@@ -2,20 +2,30 @@
 
 #include <QLabel>
 
+#include "fnd/memory.h"
+#include "fnd/NonCopyMovable.h"
+
 namespace HomeCompa::Flibrary {
 
 class ClickableLabel final : public QLabel
 {
     Q_OBJECT
+	NON_COPY_MOVABLE(ClickableLabel)
 
 public:
     explicit ClickableLabel(QWidget * parent = nullptr);
+	~ClickableLabel() override;
 
 signals:
     void clicked(const QPoint & pos);
+	void doubleClicked(const QPoint & pos);
 
 private:
     void mousePressEvent(QMouseEvent * event) override;
+
+private:
+	struct Impl;
+	PropagateConstPtr<Impl> m_impl;
 };
 
 }
