897d063d71fee434907edc5350e4fbf438688cc0
Alexander Chiryatev <heimdallrnsk@gmail.com>
2024-12-25 21:21:03 +0700

    #306 SEVERITY_DARK

--- src/home/flibrary/gui/LogItemDelegate.cpp
+++ src/home/flibrary/gui/LogItemDelegate.cpp
@@ -1,30 +1,19 @@
 #include "LogItemDelegate.h"
 
-#include <plog/Log.h>
+#include <ranges>
+
+#include <QGuiApplication>
 
 #include "interface/logic/LogModelRole.h"
-#include "util/ISettings.h"
 
 using namespace HomeCompa::Flibrary;
 
-namespace {
-constexpr auto COLOR_KEY = "ui/Colors/Log/%1";
-}
-
-LogItemDelegate::LogItemDelegate(const std::shared_ptr<ISettings> & settings
-	, QObject * parent
-)
+LogItemDelegate::LogItemDelegate(QObject * parent)
 	: QStyledItemDelegate(parent)
 {
-	std::ranges::transform(SEVERITIES, std::begin(m_colors), [&] (const auto & severity)
-	{
-		const auto key = QString(COLOR_KEY).arg(severity.first);
-		if (const auto colorInt = settings->Get(key, 0); colorInt != 0)
-			return QColor(colorInt & 0x000000FF, (colorInt & 0x0000FF00) >> 8, (colorInt & 0x00FF0000) >> 16, (colorInt & 0xFF000000) >> 24);
-
-		settings->Set(key, severity.second.red() | (severity.second.green() << 8) | (severity.second.blue() << 16) | (severity.second.alpha() << 24));
-		return severity.second;
-	});
+	const auto palette = QGuiApplication::palette();
+	const auto & severities = palette.color(QPalette::WindowText).lightness() > palette.color(QPalette::Window).lightness() ? SEVERITIES_DARK : SEVERITIES;
+	std::ranges::copy(severities | std::views::values, std::begin(m_colors));
 }
 
 void LogItemDelegate::paint(QPainter * painter, const QStyleOptionViewItem & option, const QModelIndex & index) const
--- src/home/flibrary/version/build.h
+++ src/home/flibrary/version/build.h
@@ -1,1 +1,1 @@
-constexpr int BUILD_NUMBER = 4926 ;
+constexpr int BUILD_NUMBER = 4947 ;
