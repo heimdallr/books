33f1f5a625e34a2c54d3767f64d2a2431c5b2d4c
Alexander Chiryatev <heimdallrnsk@gmail.com>
2025-01-02 22:16:16 +0700

    #316 Добавлен link safe

--- src/home/tool/opds/Requester.h
+++ src/home/tool/opds/Requester.h
@@ -26,23 +26,24 @@ public:
 	~Requester() override;
 
 private: // IRequester
-	QByteArray GetRoot() const override;
-	QByteArray GetBookInfo(const QString & bookId) const override;
-	QByteArray GetCoverThumbnail(const QString & bookId) const override;
-	QByteArray GetBook(const QString & bookId) const override;
-	QByteArray GetBookZip(const QString & bookId) const override;
+	QByteArray GetRoot(const QString & self) const override;
+	QByteArray GetBookInfo(const QString & self, const QString & bookId) const override;
+	QByteArray GetCover(const QString & self, const QString & bookId) const override;
+	QByteArray GetCoverThumbnail(const QString & self, const QString & bookId) const override;
+	QByteArray GetBook(const QString & self, const QString & bookId) const override;
+	QByteArray GetBookZip(const QString & self, const QString & bookId) const override;
 
-#define OPDS_ROOT_ITEM(NAME) QByteArray Get##NAME##Navigation(const QString & value) const override;
+#define OPDS_ROOT_ITEM(NAME) QByteArray Get##NAME##Navigation(const QString & self, const QString & value) const override;
 		OPDS_ROOT_ITEMS_X_MACRO
 #undef  OPDS_ROOT_ITEM
 
-#define OPDS_ROOT_ITEM(NAME) QByteArray Get##NAME##Books(const QString & navigationId, const QString & value) const override;
+#define OPDS_ROOT_ITEM(NAME) QByteArray Get##NAME##Books(const QString & self, const QString & navigationId, const QString & value) const override;
 		OPDS_ROOT_ITEMS_X_MACRO
 #undef  OPDS_ROOT_ITEM
 
 private:
 	template<typename NavigationGetter, typename... ARGS>
-	QByteArray GetImpl(NavigationGetter getter, const ARGS &... args) const;
+	QByteArray GetImpl(NavigationGetter getter, const QString & self, const ARGS &... args) const;
 
 private:
 	struct Impl;
