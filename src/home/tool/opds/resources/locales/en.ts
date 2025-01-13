bd30f6089a60bf887bf720bc090baa827ebcc493
Alexander Chiryatev <heimdallrnsk@gmail.com>
2025-01-14 01:19:37 +0700

    #331 Переводы

---
+++ src/home/tool/opds/resources/locales/en.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>Requester</name>
+    <message>
+        <location filename="../../Requester.cpp" line="82"/>
+        <source>Number of: %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../Requester.cpp" line="83"/>
+        <source>Book</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../Requester.cpp" line="84"/>
+        <source>Books</source>
+        <translation>All books in the section</translation>
+    </message>
+</context>
+</TS>
---
+++ src/home/tool/opds/resources/locales/ru.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>Requester</name>
+    <message>
+        <location filename="../../Requester.cpp" line="82"/>
+        <source>Number of: %1</source>
+        <translation>Количество: %1</translation>
+    </message>
+    <message>
+        <location filename="../../Requester.cpp" line="83"/>
+        <source>Book</source>
+        <translation>Книга</translation>
+    </message>
+    <message>
+        <location filename="../../Requester.cpp" line="84"/>
+        <source>Books</source>
+        <translation>Все книги раздела</translation>
+    </message>
+</context>
+</TS>
