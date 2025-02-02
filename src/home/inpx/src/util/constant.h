581c1a623a759b827a45f6b6799fbf59c3acff86
Alexander Chiryatev <heimdallrnsk@gmail.com>
2025-02-02 14:39:26 +0700

    #359 Косметика

--- src/home/inpx/src/util/constant.h
+++ src/home/inpx/src/util/constant.h
@@ -1,37 +1,37 @@
 #pragma once
 
-[[maybe_unused]] constexpr wchar_t COMMENT_START     = '#';
-[[maybe_unused]] constexpr wchar_t DATE_SEPARATOR   = '-';
-[[maybe_unused]] constexpr wchar_t FIELDS_SEPARATOR = '\x04';
-[[maybe_unused]] constexpr wchar_t GENRE_SEPARATOR  = '|';
-[[maybe_unused]] constexpr wchar_t INI_SEPARATOR    = '=';
-[[maybe_unused]] constexpr wchar_t LIST_SEPARATOR   = ':';
-[[maybe_unused]] constexpr wchar_t NAMES_SEPARATOR  = ',';
+constexpr wchar_t COMMENT_START     = '#';
+constexpr wchar_t DATE_SEPARATOR   = '-';
+constexpr wchar_t FIELDS_SEPARATOR = '\x04';
+constexpr wchar_t GENRE_SEPARATOR  = '|';
+constexpr wchar_t INI_SEPARATOR    = '=';
+constexpr wchar_t LIST_SEPARATOR   = ':';
+constexpr wchar_t NAMES_SEPARATOR  = ',';
 
-[[maybe_unused]] constexpr auto COLLECTION_INFO          = L"collection.info";
-[[maybe_unused]] constexpr auto DB_CREATE_SCRIPT         = L"db_create_script";
-[[maybe_unused]] constexpr auto DB_PATH                  = L"db_path";
-[[maybe_unused]] constexpr auto DB_UPDATE_SCRIPT         = L"db_update_script";
-[[maybe_unused]] constexpr auto DEFAULT_DB_CREATE_SCRIPT = L"CreateCollection.sql";
-[[maybe_unused]] constexpr auto DEFAULT_DB_UPDATE_SCRIPT = L"UpdateCollection.sql";
-[[maybe_unused]] constexpr auto DEFAULT_GENRES           = L"genres.lst";
-[[maybe_unused]] constexpr auto GENRES                   = L"genres";
-[[maybe_unused]] constexpr auto INI_EXT                  = L"ini";
-[[maybe_unused]] constexpr auto INPX_EXT                 = L".inpx";
-[[maybe_unused]] constexpr auto INPX_FOLDER              = L"inpx_folder";
-[[maybe_unused]] constexpr auto MHL_TRIGGERS_ON          = L"mhl_triggers_on";
-[[maybe_unused]] constexpr auto VERSION_INFO             = L"version.info";
-[[maybe_unused]] constexpr auto ZIP                      = L"zip";
+constexpr auto COLLECTION_INFO          = L"collection.info";
+constexpr auto DB_CREATE_SCRIPT         = L"db_create_script";
+constexpr auto DB_PATH                  = L"db_path";
+constexpr auto DB_UPDATE_SCRIPT         = L"db_update_script";
+constexpr auto DEFAULT_DB_CREATE_SCRIPT = L"CreateCollection.sql";
+constexpr auto DEFAULT_DB_UPDATE_SCRIPT = L"UpdateCollection.sql";
+constexpr auto DEFAULT_GENRES           = L"genres.lst";
+constexpr auto GENRES                   = L"genres";
+constexpr auto INI_EXT                  = L"ini";
+constexpr auto INPX_EXT                 = L".inpx";
+constexpr auto INPX_FOLDER              = L"inpx_folder";
+constexpr auto MHL_TRIGGERS_ON          = L"mhl_triggers_on";
+constexpr auto VERSION_INFO             = L"version.info";
+constexpr auto ZIP                      = L"zip";
 
-[[maybe_unused]] constexpr const char * SCHEMA_VERSION_VALUE = "{FEC8CB6F-300A-4b92-86D1-7B40867F782B}";
+constexpr const char * SCHEMA_VERSION_VALUE = "{FEC8CB6F-300A-4b92-86D1-7B40867F782B}";
 
-[[maybe_unused]] constexpr auto INP_EXT = L".inp";
-[[maybe_unused]] constexpr auto DATE_ADDED_CODE = L"date_added_code_root";
-[[maybe_unused]] constexpr auto NO_DATE_SPECIFIED = L"No date specified";
+constexpr auto INP_EXT = L".inp";
+constexpr auto DATE_ADDED_CODE = L"date_added_code_root";
+constexpr auto NO_DATE_SPECIFIED = L"No date specified";
 
-[[maybe_unused]] constexpr size_t LOG_INTERVAL = 10000;
+constexpr size_t LOG_INTERVAL = 10000;
 
-[[maybe_unused]] constexpr auto GET_MAX_ID_QUERY =
+constexpr auto GET_MAX_ID_QUERY =
 	"select coalesce(max(m), 0) from ("
 	"      select max(BookID)    m from Books "
 	"union select max(AuthorID)  m from Authors "
