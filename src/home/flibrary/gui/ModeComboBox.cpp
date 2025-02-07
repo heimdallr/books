6bb4b52c2eb2f67f59e87c7373d3cbc079e497ca
Alexander Chiryatev <heimdallrnsk@gmail.com>
2025-02-07 11:14:38 +0700

    #379 find.svg, filter.svg

--- src/home/flibrary/gui/ModeComboBox.cpp
+++ src/home/flibrary/gui/ModeComboBox.cpp
@@ -2,14 +2,10 @@
 
 #include <ranges>
 
-#include <QFile>
 #include <QGuiApplication>
 #include <QPainter>
-#include <QSvgRenderer>
-#include <QStyledItemDelegate>
 
 #include "fnd/ScopedCall.h"
-#include "util/ColorUtil.h"
 
 using namespace HomeCompa::Flibrary;
 
@@ -19,52 +15,12 @@ constexpr auto VALUE_MODE_ICON_TEMPLATE = ":/icons/%1.svg";
 
 }
 
-struct ModeComboBox::Impl : QStyledItemDelegate
-{
-    std::vector<std::unique_ptr<QSvgRenderer>> svgRenderers { CreateRenderers() };
-
-private:
-    void paint(QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index) const override
-    {
-        QStyledItemDelegate::paint(painter, option, index);
-        const auto adjust = option.rect.size() / 8;
-        auto rect = option.rect.adjusted(adjust.width(), adjust.height(), -adjust.width(), -adjust.height());
-        rect = rect.width() > rect.height()
-            ? rect.adjusted((rect.width() - rect.height()) / 2, 0, -(rect.width() - rect.height()) / 2, 0)
-            : rect.adjusted(0, (rect.height() - rect.width()) / 2, 0, -(rect.height() - rect.width()) / 2);
-        svgRenderers[static_cast<size_t>(index.row())]->render(painter, rect);
-    }
-
-private:
-    static std::vector< std::unique_ptr<QSvgRenderer>> CreateRenderers()
-    {
-        std::vector<std::unique_ptr<QSvgRenderer>> result;
-        std::ranges::transform(VALUE_MODES | std::views::keys, std::back_inserter(result), [] (const auto & name)
-        {
-            auto renderer = std::make_unique<QSvgRenderer>();
-            QFile file(QString(VALUE_MODE_ICON_TEMPLATE).arg(name));
-            if (!file.open(QIODevice::ReadOnly))
-                return renderer;
-
-            const auto content = QString::fromUtf8(file.readAll()).arg(HomeCompa::Util::ToString(QGuiApplication::palette(), QPalette::Text));
-            renderer->load(content.toUtf8());
-            return renderer;
-        });
-        return result;
-    }
-};
-
 ModeComboBox::ModeComboBox(QWidget *parent)
 	: QComboBox(parent)
-    , m_impl { std::make_unique<Impl>() }
 {
 	for (const auto * name : VALUE_MODES | std::views::keys)
-		addItem("", QString(name));
-
-    setItemDelegate(m_impl.get());
+		addItem(QIcon(QString(VALUE_MODE_ICON_TEMPLATE).arg(name)), QString(name));
 }
-
-ModeComboBox::~ModeComboBox() = default;
 
 void ModeComboBox::paintEvent(QPaintEvent * event)
 {
@@ -77,6 +33,8 @@ void ModeComboBox::paintEvent(QPaintEvent * event)
     style()->drawPrimitive(QStyle::PE_PanelButtonBevel, &opt, &p, this);
     style()->drawPrimitive(QStyle::PE_PanelButtonCommand, &opt, &p, this);
 
-    const auto adjust = opt.rect.size() / 8;
-    m_impl->svgRenderers[static_cast<size_t>(currentIndex())]->render(&p, opt.rect.adjusted(adjust.width(), adjust.height(), -adjust.width(), -adjust.height()));
+    QIcon icon(QString(VALUE_MODE_ICON_TEMPLATE).arg(VALUE_MODES[currentIndex()].first));
+    const auto adjust = opt.rect.size() / 6;
+    const auto adjustedRect = opt.rect.adjusted(adjust.width(), adjust.height(), -adjust.width(), -adjust.height());
+    p.drawPixmap(adjustedRect.topLeft(), icon.pixmap(adjustedRect.size()));
 }
--- src/home/flibrary/version/build.h
+++ src/home/flibrary/version/build.h
@@ -1,1 +1,1 @@
-constexpr int BUILD_NUMBER = 5866 ;
+constexpr int BUILD_NUMBER = 5880 ;
