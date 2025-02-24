6830ad288d23ae1695045f774ed3ae3c73004602
Alexander Chiryatev <heimdallrnsk@gmail.com>
2025-02-24 20:26:01 +0700

    #457 Отключаем проверку обновлений на старте

--- src/home/flibrary/gui/MainWindow.cpp
+++ src/home/flibrary/gui/MainWindow.cpp
@@ -87,6 +87,7 @@ constexpr auto SHOW_REMOVED_BOOKS_KEY = "ui/View/RemovedBooks";
 constexpr auto SHOW_STATUS_BAR_KEY = "ui/View/Status";
 constexpr auto SHOW_JOKES_KEY = "ui/View/ShowJokes";
 constexpr auto SHOW_SEARCH_BOOK_KEY = "ui/View/ShowSearchBook";
+constexpr auto CHECK_FOR_UPDATE_ON_START_KEY = "ui/View/CheckForUpdateOnStart";
 constexpr auto ACTION_PROPERTY_NAME = "value";
 constexpr auto QSS = "qss";
 
@@ -223,7 +224,8 @@ public:
 				collectionUpdateCheckerRef.CheckForUpdate([collectionUpdateChecker = std::move(collectionUpdateChecker)](bool) mutable { collectionUpdateChecker.reset(); });
 			});
 
-		CheckForUpdates(false);
+		if (m_checkForUpdateOnStartEnabled)
+			CheckForUpdates(false);
 	}
 
 	~Impl() override
@@ -306,6 +308,11 @@ private:
 
 		m_ui.lineEditBookTitleToSearch->addAction(m_ui.actionSearchBookByTitle, QLineEdit::LeadingPosition);
 
+		OnObjectVisibleChanged(this,
+		                       &Impl::SetCheckForUpdateOnStartEnabled,
+		                       m_ui.actionEnableCheckForUpdateOnStart,
+		                       m_ui.actionDisableCheckForUpdateOnStart,
+		                       m_settings->Get(CHECK_FOR_UPDATE_ON_START_KEY, m_checkForUpdateOnStartEnabled));
 		OnObjectVisibleChanged(m_booksWidget.get(), &TreeView::ShowRemoved, m_ui.actionShowRemoved, m_ui.actionHideRemoved, m_settings->Get(SHOW_REMOVED_BOOKS_KEY, true));
 		OnObjectVisibleChanged(m_ui.annotationWidget, &QWidget::setVisible, m_ui.actionShowAnnotation, m_ui.menuAnnotation->menuAction(), m_settings->Get(SHOW_ANNOTATION_KEY, true));
 		OnObjectVisibleChanged(m_annotationWidget.get(),
@@ -494,6 +501,7 @@ private:
 		ConnectShowHide(m_annotationWidget.get(), &AnnotationWidget::ShowCover, m_ui.actionShowAnnotationCover, m_ui.actionHideAnnotationCover, SHOW_ANNOTATION_COVER_KEY);
 		ConnectShowHide(m_annotationWidget.get(), &AnnotationWidget::ShowCoverButtons, m_ui.actionShowAnnotationCoverButtons, m_ui.actionHideAnnotationCoverButtons, SHOW_ANNOTATION_COVER_BUTTONS_KEY);
 		ConnectShowHide(this, &Impl::AllowDestructiveOperation, m_ui.actionAllowDestructiveOperations, m_ui.actionDenyDestructiveOperations);
+		ConnectShowHide(this, &Impl::SetCheckForUpdateOnStartEnabled, m_ui.actionEnableCheckForUpdateOnStart, m_ui.actionDisableCheckForUpdateOnStart, CHECK_FOR_UPDATE_ON_START_KEY);
 		ConnectShowHide<QStatusBar>(m_ui.statusBar, &QWidget::setVisible, m_ui.actionShowStatusBar, m_ui.actionHideStatusBar, SHOW_STATUS_BAR_KEY);
 		ConnectShowHide<QLineEdit>(m_ui.lineEditBookTitleToSearch, &QWidget::setVisible, m_ui.actionShowSearchBookString, m_ui.actionHideSearchBookString, SHOW_SEARCH_BOOK_KEY);
 
@@ -753,6 +761,11 @@ private:
 			inpxGeneratorRef.GenerateInpx(std::move(inpxFileName), [inpxGenerator = std::move(inpxGenerator)](bool) mutable { inpxGenerator.reset(); });
 	}
 
+	void SetCheckForUpdateOnStartEnabled(const bool value) noexcept
+	{
+		m_checkForUpdateOnStartEnabled = value;
+	}
+
 	static void Reboot()
 	{
 		QTimer::singleShot(0, [] { QCoreApplication::exit(Constant::RESTART_APP); });
@@ -784,6 +797,8 @@ private:
 	QMetaObject::Connection m_settingsLineEditExecuteContextMenuConnection;
 	QSpacerItem* m_searchBooksByTitleLeft;
 	QLayout* m_searchBooksByTitleLayout;
+
+	bool m_checkForUpdateOnStartEnabled { true };
 };
 
 MainWindow::MainWindow(const std::shared_ptr<const ILogicFactory>& logicFactory,
