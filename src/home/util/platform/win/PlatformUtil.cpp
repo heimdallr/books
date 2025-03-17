4e325e353436ffa584ac34bbffd3e006278d5c4d
Alexander Chiryatev <heimdallrnsk@gmail.com>
2025-03-17 19:34:01 +0700

    #527 Типа может и не быть

--- src/home/util/platform/win/PlatformUtil.cpp
+++ src/home/util/platform/win/PlatformUtil.cpp
@@ -7,10 +7,13 @@ namespace HomeCompa::Util
 
 bool IsRegisteredExtension(const QString& extension)
 {
-	const QSettings m("HKEY_CLASSES_ROOT", QSettings::NativeFormat);
+	QSettings m("HKEY_CLASSES_ROOT", QSettings::NativeFormat);
 	const auto fileType = m.value(QString(".%1/.").arg(extension)).toString();
 	if (fileType.isEmpty())
-		return false;
+	{
+		m.beginGroup(QString(".%1/OpenWithProgids").arg(extension));
+		return !m.allKeys().isEmpty();
+	}
 
 	const auto command = m.value(QString("%1/shell/open/command/.").arg(fileType)).toString();
 	return !command.isEmpty();
