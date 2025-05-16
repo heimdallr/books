49e231247fbc08bccb9707647706c0f5fcf4f155
Alexander Chiryatev <heimdallrnsk@gmail.com>
2025-05-16 21:25:29 +0700

    clang-format
    [TECH]

--- src/home/flibrary/logic/Annotation/AnnotationController.cpp
+++ src/home/flibrary/logic/Annotation/AnnotationController.cpp
@@ -55,8 +55,9 @@ TR_DEF
 using Extractor = IDataItem::Ptr (*)(const DB::IQuery& query, const size_t* index, size_t removedIndex);
 constexpr size_t QUERY_INDEX_SIMPLE_LIST_ITEM[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-constexpr auto BOOK_QUERY = "select %1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, coalesce(b.SeqNumber, -1), s.SeriesTitle from Books b join Folders f on f.FolderID = b.FolderID left join Books_User bu on bu.BookID = b.BookID "
-                            "left join Series s on s.SeriesID = b.SeriesID where b.BookID = :id";
+constexpr auto BOOK_QUERY =
+	"select %1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, coalesce(b.SeqNumber, -1), s.SeriesTitle from Books b join Folders f on f.FolderID = b.FolderID left join Books_User bu on bu.BookID = b.BookID "
+	"left join Series s on s.SeriesID = b.SeriesID where b.BookID = :id";
 constexpr auto SERIES_QUERY = "select s.SeriesID, s.SeriesTitle from Series s join Series_List sl on sl.SeriesID = s.SeriesID and sl.BookID = :id";
 constexpr auto AUTHORS_QUERY = "select a.AuthorID, a.LastName, a.LastName, a.FirstName, a.MiddleName from Authors a  join Author_List al on al.AuthorID = a.AuthorID and al.BookID = :id";
 constexpr auto GENRES_QUERY = "select g.GenreCode, g.GenreAlias from Genres g join Genre_List gl on gl.GenreCode = g.GenreCode and gl.BookID = :id";
