d64655f972dd1b510e6b484230ce19dbad9e4508
Alexander Chiryatev <heimdallrnsk@gmail.com>
2025-06-17 02:02:27 +0700

    #734 Диалог выбора жанров

---
+++ src/home/flibrary/gui/dialogs/GenreFilterDialog.cpp
@@ -0,0 +1,124 @@
+#include "ui_GenreFilterDialog.h"
+
+#include "GenreFilterDialog.h"
+
+#include "interface/logic/IModel.h"
+
+#include "GuiUtil/GeometryRestorable.h"
+
+using namespace HomeCompa;
+using namespace Flibrary;
+
+namespace
+{
+
+std::set<QString> GetSelected(const QAbstractItemModel& model)
+{
+	auto list = model.data({}, IGenreModel::Role::SelectedList).toStringList();
+	std::set<QString> selected;
+	std::ranges::move(list, std::inserter(selected, selected.end()));
+	return selected;
+}
+
+}
+
+struct GenreFilterDialog::Impl final
+	: Util::GeometryRestorable
+	, Util::GeometryRestorableObserver
+{
+	PropagateConstPtr<IGenreFilterController, std::shared_ptr> genreFilterController;
+	PropagateConstPtr<IGenreModel, std::shared_ptr> genreModel;
+	std::set<QString> selected;
+	std::unordered_set<QString> filtered { genreFilterController->GetFilteredGenreCodes() };
+	Ui::GenreFilterDialog ui {};
+
+	Impl(GenreFilterDialog& self,
+	     std::unordered_set<QString> visibleGenres,
+	     std::shared_ptr<ISettings> settings,
+	     std::shared_ptr<IGenreFilterController> genreFilterController,
+	     std::shared_ptr<IGenreModel> genreModel)
+		: GeometryRestorable(*this, std::move(settings), "GenreFilterDialog")
+		, GeometryRestorableObserver(self)
+		, genreFilterController { std::move(genreFilterController) }
+		, genreModel { std::move(genreModel) }
+	{
+		ui.setupUi(&self);
+
+		auto* model = this->genreModel->GetModel();
+
+		connect(model,
+		        &QAbstractItemModel::modelReset,
+		        [this, model]
+		        {
+					model->setData({}, {}, IGenreModel::Role::CheckAll);
+					if (!filtered.empty())
+						model->setData({}, QVariant::fromValue(filtered), IGenreModel::Role::UncheckAll);
+					selected = GetSelected(*model);
+				});
+		model->setData({}, QVariant::fromValue(std::move(visibleGenres)), IGenreModel::Role::VisibleGenreCodes);
+		ui.view->setModel(model);
+
+		LoadGeometry();
+	}
+
+	~Impl() override
+	{
+		SaveGeometry();
+	}
+
+	void Save()
+	{
+		const auto current = GetSelected(*genreModel->GetModel());
+		for (const auto& item : current)
+			filtered.erase(item);
+
+		std::ranges::set_difference(selected, current, std::inserter(filtered, filtered.end()));
+		genreFilterController->SetFilteredGenres(filtered);
+	}
+
+	NON_COPY_MOVABLE(Impl)
+};
+
+GenreFilterDialog::GenreFilterDialog(const std::shared_ptr<const IUiFactory>& uiFactory,
+                                     std::shared_ptr<ISettings> settings,
+                                     std::shared_ptr<IGenreFilterController> genreFilterController,
+                                     std::shared_ptr<IGenreModel> genreModel,
+                                     QWidget* parent)
+	: QDialog(uiFactory->GetParentWidget(parent))
+	, m_impl(*this, uiFactory->GetVisibleGenres(), std::move(settings), std::move(genreFilterController), std::move(genreModel))
+{
+	connect(this, &QDialog::accepted, [this] { m_impl->Save(); });
+}
+
+GenreFilterDialog::~GenreFilterDialog() = default;
+
+/*
+std::unordered_set<QString> ModelProvider::GetFilteredGenres() const
+{
+	auto list = m_impl->container.resolve<ISettings>()->Get(Constant::Settings::FILTERED_GENRES_KEY).toStringList();
+	if (list.isEmpty())
+		return {};
+
+	if (m_impl->genreCodeToName.empty())
+	{
+		const auto enumerate = [&genreCodeToName = m_impl->genreCodeToName](const Genre& genre, const auto& f) -> void
+		{
+			genreCodeToName.try_emplace(genre.code, genre.name);
+			for (const auto& child : genre.children)
+				f(child, f);
+		};
+		enumerate(GetGenres(), enumerate);
+	}
+
+	std::unordered_set<QString> result;
+	std::ranges::transform(list,
+	                       std::inserter(result, result.end()),
+	                       [&genreCodeToName = m_impl->genreCodeToName](const QString& code)
+	                       {
+							   const auto it = genreCodeToName.find(code);
+							   return it != genreCodeToName.end() ? it->second : QString {};
+						   });
+
+	return result;
+}
+*/
