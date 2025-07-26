4555993cf1b9dbf2798f67c13a148fa4b0018efa
Alexander Chiryatev <heimdallrnsk@gmail.com>
2025-07-26 21:26:03 +0700

    #759 ICU wrapper. Обёртка над transliterate

---
+++ src/home/icu/icu.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include <string>
+
+#include "export/icu.h"
+
+namespace HomeCompa::ICU
+{
+static constexpr auto LIB_NAME = "icu";
+static constexpr auto TRANSLITERATE_NAME = "ICU_Transliterate";
+using TransliterateType = void (*)(const char* id, const std::u32string* src, std::u32string* dst);
+}
+
+extern "C" ICU_EXPORT void ICU_Transliterate(const char* id, const std::u32string* src, std::u32string* dst);
