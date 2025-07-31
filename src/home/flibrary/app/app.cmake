111e7e1b580dc9ba443d41fb03fc52c917a35789
Alexander Chiryatev <heimdallrnsk@gmail.com>
2025-07-31 23:58:32 +0700

    #267 Колхоз светлый путь. Но чот не могу найти как нормально сделать, без хардкода

--- src/home/flibrary/app/app.cmake
+++ src/home/flibrary/app/app.cmake
@@ -6,20 +6,38 @@ CreateWinRC(app
     	APP_VERSION       ${PRODUCT_VERSION}
 )
 
-if (${CMAKE_BUILD_TYPE} STREQUAL "Release")
-	file(WRITE "${CMAKE_BINARY_DIR}/config/installer_mode" "msi")
-	install(FILES "${CMAKE_BINARY_DIR}/config/installer_mode" DESTINATION .)
-endif()
-
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/../../../../LICENSE_en.txt" DESTINATION ${CMAKE_BINARY_DIR}/bin)
 file(RENAME "${CMAKE_BINARY_DIR}/bin/LICENSE_en.txt" "${CMAKE_BINARY_DIR}/bin/LICENSE.txt")
 
-install(DIRECTORY ${CMAKE_BINARY_DIR}/bin/imageformats DESTINATION .)
-install(DIRECTORY ${CMAKE_BINARY_DIR}/bin/locales DESTINATION .)
-install(DIRECTORY ${CMAKE_BINARY_DIR}/bin/platforms DESTINATION .)
-install(DIRECTORY ${CMAKE_BINARY_DIR}/bin/styles DESTINATION .)
-install(DIRECTORY ${CMAKE_BINARY_DIR}/bin/tls DESTINATION .)
-install(FILES ${CMAKE_BINARY_DIR}/bin/LICENSE.txt DESTINATION .)
+if (${CMAKE_BUILD_TYPE} STREQUAL "Release")
+	file(WRITE "${CMAKE_BINARY_DIR}/config/installer_mode" "msi")
+	
+#Да, колхоз. Но я устал
+	set(QT_BIN_FILES
+		${QT6_INSTALL_PREFIX}/${QT6_INSTALL_BINS}/Qt6Core.dll
+		${QT6_INSTALL_PREFIX}/${QT6_INSTALL_BINS}/Qt6Gui.dll
+		${QT6_INSTALL_PREFIX}/${QT6_INSTALL_BINS}/Qt6Network.dll
+		${QT6_INSTALL_PREFIX}/${QT6_INSTALL_BINS}/Qt6Svg.dll
+		${QT6_INSTALL_PREFIX}/${QT6_INSTALL_BINS}/Qt6Widgets.dll
+		${QT6_INSTALL_PREFIX}/${QT6_INSTALL_BINS}/Qt6HttpServer.dll
+		${QT6_INSTALL_PREFIX}/${QT6_INSTALL_BINS}/Qt6WebSockets.dll
+	)
+	file(COPY ${QT_BIN_FILES} DESTINATION ${CMAKE_BINARY_DIR}/bin)
+
+	install(DIRECTORY ${CMAKE_BINARY_DIR}/bin/imageformats DESTINATION .)
+	install(DIRECTORY ${CMAKE_BINARY_DIR}/bin/locales DESTINATION .)
+	install(DIRECTORY ${CMAKE_BINARY_DIR}/bin/platforms DESTINATION .)
+	install(DIRECTORY ${CMAKE_BINARY_DIR}/bin/styles DESTINATION .)
+	install(DIRECTORY ${CMAKE_BINARY_DIR}/bin/tls DESTINATION .)
+	install(
+		FILES
+			"${CMAKE_BINARY_DIR}/config/installer_mode"
+			"${CMAKE_BINARY_DIR}/bin/LICENSE.txt"
+			${QT_BIN_FILES}
+		DESTINATION .
+		)
+endif()
+
 
 AddTarget(${PROJECT_NAME}	app
 	PROJECT_GROUP App
