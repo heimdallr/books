41f0cde64858e4dbae85857d30eb928ef35350b4
Alexander Chiryatev <heimdallrnsk@gmail.com>
2025-08-01 01:44:30 +0700

    Баг. Сначала надо заальтерить таблицу, а только потом добавлять с ней вьюху

--- src/home/flibrary/logic/database/DatabaseScheme.cpp
+++ src/home/flibrary/logic/database/DatabaseScheme.cpp
@@ -219,36 +219,6 @@ AS SELECT
 	FROM Books b
 	LEFT JOIN Books_User bu ON bu.BookID = b.BookID
 )",
-	R"(
-CREATE VIEW IF NOT EXISTS Groups_List_User_View (
-    GroupID,
-    BookID
-)
-AS
-    SELECT glu.GroupID,
-           b.BookID
-      FROM Groups_List_User glu
-           JOIN
-           Books b ON b.BookID = glu.ObjectID
-    UNION
-    SELECT glu.GroupID,
-           al.BookID
-      FROM Groups_List_User glu
-           JOIN
-           Author_List al ON al.AuthorID = glu.ObjectID
-    UNION
-    SELECT glu.GroupID,
-           sl.BookID
-      FROM Groups_List_User glu
-           JOIN
-           Series_List sl ON sl.SeriesID = glu.ObjectID
-    UNION
-    SELECT glu.GroupID,
-           kl.BookID
-      FROM Groups_List_User glu
-           JOIN
-           Keyword_List kl ON kl.KeywordID = glu.ObjectID
-)",
 	};
 	// clang-format on
 
@@ -328,6 +298,36 @@ void AddTableFields(DB::ITransaction& transaction)
 						  "DROP TABLE Groups_List_User",
 						  "ALTER TABLE Groups_List_User_Tmp RENAME TO Groups_List_User",
 						  "CREATE INDEX IX_Groups_List_User_ObjectID ON Groups_List_User(ObjectID)",
+						  R"(
+CREATE VIEW IF NOT EXISTS Groups_List_User_View (
+    GroupID,
+    BookID
+)
+AS
+    SELECT glu.GroupID,
+           b.BookID
+      FROM Groups_List_User glu
+           JOIN
+           Books b ON b.BookID = glu.ObjectID
+    UNION
+    SELECT glu.GroupID,
+           al.BookID
+      FROM Groups_List_User glu
+           JOIN
+           Author_List al ON al.AuthorID = glu.ObjectID
+    UNION
+    SELECT glu.GroupID,
+           sl.BookID
+      FROM Groups_List_User glu
+           JOIN
+           Series_List sl ON sl.SeriesID = glu.ObjectID
+    UNION
+    SELECT glu.GroupID,
+           kl.BookID
+      FROM Groups_List_User glu
+           JOIN
+           Keyword_List kl ON kl.KeywordID = glu.ObjectID
+)",
 					  });
 }
 
@@ -337,8 +337,8 @@ void Update(DB::IDatabase& db, const ICollectionProvider& collectionProvider)
 {
 	const auto transaction = db.CreateTransaction();
 	DropTriggers(*transaction);
-	AddTableFields(*transaction);
 	AddUserTables(*transaction);
+	AddTableFields(*transaction);
 	FixSearches_User(*transaction);
 	FillSearchTables(*transaction);
 	FillSeriesList(*transaction);
