e60b18cd2a22301021ed1a9fecd0db619c66e988
Alexander Chiryatev <heimdallrnsk@gmail.com>
2025-08-10 17:00:57 +0700

    ﻿#780 Обобщение HasAlpha (неудачное название, кстати)

--- src/home/flibrary/logic/shared/ImageRestore.h
+++ src/home/flibrary/logic/shared/ImageRestore.h
@@ -5,18 +5,15 @@
 
 #include "export/logic.h"
 
-namespace HomeCompa
-{
-class ISettings;
-}
-
 class QByteArray;
+class QImage;
 class QIODevice;
 class QPixmap;
 class QString;
 
 namespace HomeCompa
 {
+class ISettings;
 class Zip;
 }
 
@@ -29,6 +26,7 @@ constexpr auto IMAGE_PNG = "image/png";
 LOGIC_EXPORT QPixmap Decode(const QByteArray& bytes);
 LOGIC_EXPORT std::pair<QByteArray, const char*> Recode(const QByteArray& bytes);
 LOGIC_EXPORT QByteArray RestoreImages(QIODevice& input, const QString& folder, const QString& fileName, const std::shared_ptr<const ISettings>& settings = {});
+LOGIC_EXPORT QImage HasAlpha(const QImage& image, const char* data);
 
 using ExtractBookImagesCallback = std::function<bool(QString, QByteArray)>;
 void ExtractBookImages(const QString& folder, const QString& fileName, const ExtractBookImagesCallback& callback, const std::shared_ptr<const ISettings>& settings = {});
