4ebe6c9875ed595735edcfd4714e48f20662899e
Alexander Chiryatev <heimdallrnsk@gmail.com>
2025-09-03 23:37:10 +0700

    #819 Показываем новый столбец в таблице книг

--- src/home/flibrary/interface/logic/IDatabaseUser.h
+++ src/home/flibrary/interface/logic/IDatabaseUser.h
@@ -24,8 +24,9 @@ public:
 	};
 
 public:
-	static constexpr auto BOOKS_QUERY_FIELDS = "b.BookID, b.Title, coalesce(%1.SeqNumber, -1), b.UpdateDate, b.LibRate, b.Lang, f.FolderTitle, b.FileName || b.Ext, b.BookSize, coalesce(bu.userRate, 0), "
-											   "coalesce(bu.IsDeleted, b.IsDeleted, 0), b.FolderID, b.UpdateID, b.LibID";
+	static constexpr auto BOOKS_QUERY_FIELDS =
+		"b.BookID, b.Title, coalesce(%1.SeqNumber, -1), b.UpdateDate, b.LibRate, b.Lang, b.Year, f.FolderTitle, b.FileName || b.Ext, b.BookSize, coalesce(bu.userRate, 0), "
+		"coalesce(bu.IsDeleted, b.IsDeleted, 0), b.FolderID, b.UpdateID, b.LibID";
 	static constexpr auto SELECT_LAST_ID_QUERY = "select last_insert_rowid()";
 
 	FLINT_EXPORT static QString GetDatabaseVersionStatement();
@@ -54,6 +55,7 @@ struct BookQueryFields
 		UpdateDate,
 		LibRate,
 		Lang,
+		Year,
 		Folder,
 		FileName,
 		Size,
