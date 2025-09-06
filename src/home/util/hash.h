e91d46c7e231e0d2da0f355ea303218fffd555a1
Alexander Chiryatev <heimdallrnsk@gmail.com>
2025-09-06 18:18:01 +0700

    #826 Функция получения хэша по данным аутентификации

--- src/home/util/hash.h
+++ src/home/util/hash.h
@@ -8,5 +8,7 @@ namespace HomeCompa::Util
 {
 
 UTIL_EXPORT QString md5(const QByteArray& data);
+UTIL_EXPORT QString GetSaltedHash(const QString& auth);
+UTIL_EXPORT QString GetSaltedHash(const QString& user, const QString& password);
 
 }
