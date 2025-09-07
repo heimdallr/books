7b434e6d5b8980f2e34b5cee1ad30d3599e9e1a4
Alexander Chiryatev <heimdallrnsk@gmail.com>
2025-09-07 13:20:02 +0700

    #833 Тема windows11 не влазит из-за отступа в начале. Чтож, добавим с запасом

--- src/home/flibrary/gui/widgets/FComboBox.cpp
+++ src/home/flibrary/gui/widgets/FComboBox.cpp
@@ -9,11 +9,11 @@ namespace
 
 int GetMinimumWidth(const QComboBox& self)
 {
-	QFontMetrics metrics(self.view()->font());
+	const QFontMetrics metrics(self.view()->font());
 	int max = 0;
 	for (int i = 0, sz = self.count(); i < sz; ++i)
 		max = std::max(max, metrics.boundingRect(self.itemText(i)).width());
-	return max + self.style()->pixelMetric(QStyle::PM_ScrollBarExtent);
+	return max + 3 * self.style()->pixelMetric(QStyle::PM_ScrollBarExtent);
 }
 
 }
