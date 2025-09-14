7602af60bbc4cafc1230547c4bb75568f40e3067
Alexander Chiryatev <heimdallrnsk@gmail.com>
2025-09-14 11:47:50 +0700

    #848 Забытая роль HeaderName для Review-моделей

--- src/home/flibrary/logic/model/ReviewListModel.cpp
+++ src/home/flibrary/logic/model/ReviewListModel.cpp
@@ -22,22 +22,24 @@ ReviewListModel::~ReviewListModel()
 QVariant ReviewListModel::headerData(const int section, const Qt::Orientation orientation, const int role) const
 {
 	if (orientation != Qt::Horizontal)
-		return {};
+		return ListModel::headerData(section, orientation, role);
+
+	const auto getHeader = [&] { return section < m_data->GetColumnCount() ? m_data->GetData(section) : m_data->GetRawData(section - m_data->GetColumnCount() + BookItem::Column::Last); };
 
 	switch (role)
 	{
 		case Qt::DisplayRole:
 		case Role::HeaderTitle:
-		{
-			const auto data = section < m_data->GetColumnCount() ? m_data->GetData(section) : m_data->GetRawData(section - m_data->GetColumnCount() + BookItem::Column::Last);
-			return Loc::Tr(Loc::Ctx::BOOK, data.toUtf8().data());
-		}
+			return Loc::Tr(Loc::Ctx::BOOK, getHeader().toUtf8().data());
+
+		case Role::HeaderName:
+			return getHeader();
 
 		default:
 			break;
 	}
 
-	return {};
+	return ListModel::headerData(section, orientation, role);
 }
 
 int ReviewListModel::columnCount(const QModelIndex& /*parent*/) const
