d4d79875a6ed1bca8bb179773976a09bd5a57a00
Alexander Chiryatev <heimdallrnsk@gmail.com>
2025-09-22 17:23:27 +0700

    #860 Аннотация: не выбираем отфильтрованные элементы навигации

--- src/home/flibrary/logic/Annotation/AnnotationController.cpp
+++ src/home/flibrary/logic/Annotation/AnnotationController.cpp
@@ -54,11 +54,12 @@ TR_DEF
 
 using Extractor = IDataItem::Ptr (*)(const DB::IQuery& query);
 
-constexpr auto SERIES_QUERY = "select s.SeriesID, s.SeriesTitle, sl.SeqNumber from Series s join Series_List sl on sl.SeriesID = s.SeriesID and sl.BookID = :id order by sl.OrdNum";
-constexpr auto AUTHORS_QUERY = "select a.AuthorID, a.LastName, a.LastName, a.FirstName, a.MiddleName from Authors a join Author_List al on al.AuthorID = a.AuthorID and al.BookID = :id order by al.OrdNum";
-constexpr auto GENRES_QUERY = "select g.GenreCode, g.GenreAlias from Genres g join Genre_List gl on gl.GenreCode = g.GenreCode and gl.BookID = :id order by gl.OrdNum";
+constexpr auto SERIES_QUERY = "select s.SeriesID, s.SeriesTitle, sl.SeqNumber from Series s join Series_List sl on sl.SeriesID = s.SeriesID and sl.BookID = :id where s.Flags & 1 = 0 order by sl.OrdNum";
+constexpr auto AUTHORS_QUERY =
+	"select a.AuthorID, a.LastName, a.LastName, a.FirstName, a.MiddleName from Authors a join Author_List al on al.AuthorID = a.AuthorID and al.BookID = :id where a.Flags & 1 = 0 order by al.OrdNum";
+constexpr auto GENRES_QUERY = "select g.GenreCode, g.GenreAlias from Genres g join Genre_List gl on gl.GenreCode = g.GenreCode and gl.BookID = :id  where g.Flags & 1 = 0 order by gl.OrdNum";
 constexpr auto GROUPS_QUERY = "select g.GroupID, g.Title from Groups_User g join Groups_List_User_View gl on gl.GroupID = g.GroupID and gl.BookID = :id";
-constexpr auto KEYWORDS_QUERY = "select k.KeywordID, k.KeywordTitle from Keywords k join Keyword_List kl on kl.KeywordID = k.KeywordID and kl.BookID = :id order by kl.OrdNum";
+constexpr auto KEYWORDS_QUERY = "select k.KeywordID, k.KeywordTitle from Keywords k join Keyword_List kl on kl.KeywordID = k.KeywordID and kl.BookID = :id where k.Flags & 1 = 0 order by kl.OrdNum";
 constexpr auto REVIEWS_QUERY = "select b.LibID, r.Folder from Reviews r join Books b on b.BookID = r.BookID where r.BookID = :id";
 constexpr auto FOLDER_QUERY = "select f.FolderID, f.FolderTitle from Folders f join Books b on b.FolderID = f.FolderID and b.BookID = :id";
 constexpr auto UPDATE_QUERY = "select u.UpdateID, b.UpdateDate from Updates u join Books b on b.UpdateID = u.UpdateID and b.BookID = :id";
