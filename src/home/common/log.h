74663998883dad063d02e9b96264df4926109391
Alexander Chiryatev <heimdallrnsk@gmail.com>
2025-11-08 21:19:57 +0700

    #946 build fix

--- src/home/common/log.h
+++ src/home/common/log.h
@@ -1,5 +1,17 @@
 #pragma once
 
+#include <filesystem>
+
 #include <QString>
 
 #include <plog/Log.h>
+
+namespace plog
+{
+
+inline Record& operator<<(Record& record, const std::filesystem::path& /*path*/)
+{
+	return record;
+}
+
+}
