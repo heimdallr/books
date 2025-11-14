650b85a64f2620bfb83222ea8df77f0bb910d5b2
Alexander Chiryatev <heimdallrnsk@gmail.com>
2025-11-14 22:51:24 +0700

    #910 BookExtractor обучен добывать автора

--- src/home/flibrary/logic/extract/BookExtractor.cpp
+++ src/home/flibrary/logic/extract/BookExtractor.cpp
@@ -10,6 +10,7 @@
 #include "util/translit.h"
 
 #include "log.h"
+#include "data/DataItem.h"
 
 using namespace HomeCompa;
 using namespace Flibrary;
@@ -32,46 +33,84 @@ struct BookExtractor::Impl
 {
 	std::shared_ptr<const ISettings>           settings;
 	std::shared_ptr<const ICollectionProvider> collectionProvider;
-	std::shared_ptr<const IDatabaseController> databaseController;
+	std::shared_ptr<const IDatabaseUser>       databaseUser;
 	const QString                              m_outputFileNameTemplate { GetOutputFileNameTemplate(*settings) };
 
-	Impl(std::shared_ptr<const ISettings> settings, std::shared_ptr<const ICollectionProvider> collectionProvider, std::shared_ptr<const IDatabaseController> databaseController)
+	Impl(std::shared_ptr<const ISettings> settings, std::shared_ptr<const ICollectionProvider> collectionProvider, std::shared_ptr<const IDatabaseUser> databaseUser)
 		: settings { std::move(settings) }
 		, collectionProvider { std::move(collectionProvider) }
-		, databaseController { std::move(databaseController) }
+		, databaseUser { std::move(databaseUser) }
 	{
 	}
 
 	ILogicFactory::ExtractedBook GetExtractedBook(const QString& bookId) const
 	{
-		const auto db    = databaseController->GetDatabase(true);
+		const auto db    = databaseUser->Database();
 		const auto query = db->CreateQuery(R"(
 select
     f.FolderTitle,
     b.FileName||b.Ext,
     b.BookSize,
-    (select a.LastName ||' '||a.FirstName ||' '||a.MiddleName from Authors a join Author_List al on al.AuthorID = a.AuthorID and al.BookID = b.BookID order by al.OrdNum limit 1),
     s.SeriesTitle,
     b.SeqNumber,
-    b.Title
+    b.Title,
+    a.FirstName, a.MiddleName, a.LastName
 from Books b
 join Folders f on f.FolderID = b.FolderID
+join Author_List al on al.BookID = b.BookID
+join Authors a on a.AuthorID = al.AuthorID
 left join Series s on s.SeriesID = b.SeriesID
-where b.BookID = ?
+where b.BookID = 1
+order by al.OrdNum limit 1
 )");
 		query->Bind(0, bookId.toLongLong());
 		query->Execute();
 		if (query->Eof())
 			return {};
 
-		return ILogicFactory::ExtractedBook { bookId.toInt(),     query->Get<const char*>(0), query->Get<const char*>(1), query->Get<long long>(2), query->Get<const char*>(3), query->Get<const char*>(4),
-			                                  query->Get<int>(5), query->Get<const char*>(6) };
+		auto result = ILogicFactory::ExtractedBook {
+			.id        = bookId.toLongLong(),
+			.folder    = query->Get<const char*>(0),
+			.file      = query->Get<const char*>(1),
+			.size      = query->Get<long long>(2),
+			.series    = query->Get<const char*>(3),
+			.seqNumber = query->Get<int>(4),
+			.title     = query->Get<const char*>(5),
+			.authorFull = { .firstName = query->Get<const char*>(6), .middleName = query->Get<const char*>(7), .lastName = query->Get<const char*>(8), },
+		};
+
+		result.author = result.authorFull.lastName;
+		AppendTitle(result.author, result.authorFull.firstName);
+		AppendTitle(result.author, result.authorFull.middleName);
+
+		return result;
+	}
+
+	ILogicFactory::ExtractedBook::Author GetExtractedBookAuthor(const QString& bookId) const
+	{
+		const auto db    = databaseUser->Database();
+		const auto query = db->CreateQuery(R"(
+select a.FirstName, a.MiddleName, a.LastName
+from Authors a
+join Author_List al on al.AuthorID = a.AuthorID and al.BookID = ?
+order by al.OrdNum limit 1
+)");
+		query->Bind(0, bookId.toLongLong());
+		query->Execute();
+		if (query->Eof())
+			return {};
+
+		return {
+			.firstName  = query->Get<const char*>(0),
+			.middleName = query->Get<const char*>(1),
+			.lastName   = query->Get<const char*>(2),
+		};
 	}
 
 	QString GetFileName(const ILogicFactory::ExtractedBook& book) const
 	{
 		auto outputFileName = m_outputFileNameTemplate;
-		auto db             = databaseController->GetDatabase(true);
+		auto db             = databaseUser->Database();
 		ILogicFactory::FillScriptTemplate(*db, outputFileName, book);
 		if (!settings->Get(OPDS_TRANSLITERATE, false))
 			return outputFileName;
@@ -100,8 +139,8 @@ private:
 	mutable ICU::TransliterateType       m_icuTransliterate { nullptr };
 };
 
-BookExtractor::BookExtractor(std::shared_ptr<const ISettings> settings, std::shared_ptr<const ICollectionProvider> collectionProvider, std::shared_ptr<const IDatabaseController> databaseController)
-	: m_impl { std::move(settings), std::move(collectionProvider), std::move(databaseController) }
+BookExtractor::BookExtractor(std::shared_ptr<const ISettings> settings, std::shared_ptr<const ICollectionProvider> collectionProvider, std::shared_ptr<const IDatabaseUser> databaseUser)
+	: m_impl { std::move(settings), std::move(collectionProvider), std::move(databaseUser) }
 {
 }
 
@@ -121,4 +160,9 @@ QString BookExtractor::GetFileName(const ILogicFactory::ExtractedBook& book) con
 ILogicFactory::ExtractedBook BookExtractor::GetExtractedBook(const QString& bookId) const
 {
 	return m_impl->GetExtractedBook(bookId);
+}
+
+ILogicFactory::ExtractedBook::Author BookExtractor::GetExtractedBookAuthor(const QString& bookId) const
+{
+	return m_impl->GetExtractedBookAuthor(bookId);
 }
