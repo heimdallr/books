c5d73680be03bd63fabfef88aeabfe6c59aa3bc2
Alexander Chiryatev <heimdallrnsk@gmail.com>
2025-12-13 12:29:51 +0700

    #981 Alphabets/LinkedControl

--- src/home/flibrary/gui/AlphabetPanel.cpp
+++ src/home/flibrary/gui/AlphabetPanel.cpp
@@ -34,7 +34,7 @@ TR_DEF
 
 constexpr auto ID             = "id";
 constexpr auto GROUP_KEY      = "ui/View/Alphabets";
-constexpr auto LINKED_CONTROL = "ui/View/Alphabets/LinkedControl";
+constexpr auto LINKED_CONTROL = "Preferences/Alphabets/LinkedControl";
 constexpr auto KEY_TEMPLATE   = "ui/View/Alphabets/%1/%2";
 constexpr auto VISIBLE        = "visible";
 constexpr auto ORD_NUM        = "order";
