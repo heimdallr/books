5fce9914e9471bcf48f0cc7e8a89b48616e8a576
Alexander Chiryatev <heimdallrnsk@gmail.com>
2026-03-15 10:43:01 +0700

    #1108 После удаления данных таблицу поиска по аннотациям надо тоже пересчитывать

--- src/home/flibrary/logic/Collection/CollectionCleaner.cpp
+++ src/home/flibrary/logic/Collection/CollectionCleaner.cpp
@@ -110,6 +110,7 @@ not exists (select 42 from Keywords k where k.KeywordID = Groups_List_User.Objec
 		{      "Authors_Search",														  "insert into Authors_Search(Authors_Search) values('rebuild')" },
 		{       "Series_Search",															"insert into Series_Search(Series_Search) values('rebuild')" },
 		{ "Compilations_Search",												"insert into Compilations_Search(Compilations_Search) values('rebuild')" },
+		{  "Annotations_Search",												  "insert into Annotations_Search(Annotations_Search) values('rebuild')" },
 	};
 	return std::accumulate(std::cbegin(commands), std::cend(commands), true, [&](const bool init, const auto& command) {
 		PLOGD << "removing from " << command.first;
