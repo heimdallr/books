d7cabe596e73caaf53385c921e34214269f8e17c
Alexander Chiryatev <heimdallrnsk@gmail.com>
2026-03-16 14:13:28 +0700

    Мелкие правки

--- configure.bat
+++ configure.bat
@@ -16,7 +16,6 @@ del %BUILD_DIR%\*.sln
 
 cmake -B %BUILD_DIR% ^
 --no-warn-unused-cli ^
--DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
 -DQt6_DIR=D:/sdk/Qt/Qt6/6.10.0/msvc2022_64/lib/cmake/Qt6 ^
 -D7zip_BIN_DIR=D:/sdk/7z/x64/bin ^
 %* ^
