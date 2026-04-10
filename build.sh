f3f8e77ae2c4a7f28efac3b478d8bd2dc8b79988
Alexander Chiryatiev <heimdallrnsk@gmail.com>
2026-04-10 18:54:47 +0700

    TXZ самый могучий!

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DQt6_DIR=/opt/Qt/6.11.0/gcc15.2_Release/lib/cmake/Qt6 -DCPACK_GENERATOR=DEB -G Ninja
+cmake .. -DCMAKE_BUILD_TYPE=Release -DQt6_DIR=/opt/Qt/6.11.0/gcc15.2_Release/lib/cmake/Qt6 -DCPACK_GENERATOR=TXZ -G Ninja
 cmake --build . --config Release
 cpack
--- src/home/flibrary/version/build.h
+++ src/home/flibrary/version/build.h
@@ -1,1 +1,1 @@
-constexpr int BUILD_NUMBER = 17706;
+constexpr int BUILD_NUMBER = 17715;
