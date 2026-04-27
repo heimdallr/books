39110802935050d037656d76658d116b765fc822
Alexander Chiryatiev <heimdallrnsk@gmail.com>
2026-04-27 16:38:05 +0700

    linux build

--- src/home/flibrary/version/build.h
+++ src/home/flibrary/version/build.h
@@ -1,1 +1,1 @@
-constexpr int BUILD_NUMBER = 18175;
+constexpr int BUILD_NUMBER = 18191;
--- src/home/script/cmake/pack.cmake
+++ src/home/script/cmake/pack.cmake
@@ -21,8 +21,6 @@ endfunction()
 
 function(__Pack_Archive)
 	set(CPACK_ARCHIVE_FILE_NAME ${CPACK_PACKAGE_FILE_NAME})
-	file(WRITE "${CMAKE_BINARY_DIR}/start.sh" "#!/bin/bash\nLD_LIBRARY_PATH=$(dirname \"\${BASH_SOURCE[0]}\")/lib:$LD_LIBRARY_PATH $(dirname \"\${BASH_SOURCE[0]}\")/${PROJECT_NAME}")
-	install(PROGRAMS "${CMAKE_BINARY_DIR}/start.sh" DESTINATION .)
 endfunction()
 
 if("${CPACK_GENERATOR}" STREQUAL "")
@@ -42,6 +40,11 @@ set(CPACK_PACKAGE_INSTALL_DIRECTORY "FLibrary")
 set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CMAKE_PROJECT_VERSION}-setup-${OS_NAME}")
 file(WRITE "${CMAKE_BINARY_DIR}/bin/installer_mode" ${CPACK_GENERATOR})
 install(FILES "${CMAKE_BINARY_DIR}/bin/installer_mode" DESTINATION .)
+
+if(NOT WIN32)
+	file(WRITE "${CMAKE_BINARY_DIR}/start.sh" "#!/bin/bash\nLD_LIBRARY_PATH=$(dirname \"\${BASH_SOURCE[0]}\")/lib:$LD_LIBRARY_PATH ICU_DATA=$(dirname \"\${BASH_SOURCE[0]}\")/lib $(dirname \"\${BASH_SOURCE[0]}\")/${PROJECT_NAME}")
+	install(PROGRAMS "${CMAKE_BINARY_DIR}/start.sh" DESTINATION .)
+endif()
 
 if("${CPACK_GENERATOR}" STREQUAL "WIX")
 	__Pack_WIX()
