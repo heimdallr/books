27fafcb3ea1d0b9f6d10e6e95a4bba34b55e8f75
Alexander Chiryatev <heimdallrnsk@gmail.com>
2026-05-07 01:37:51 +0700

    pullup scripts

--- src/home/flibrary/version/build.h
+++ src/home/flibrary/version/build.h
@@ -1,1 +1,1 @@
-constexpr int BUILD_NUMBER = 18436;
+constexpr int BUILD_NUMBER = 18437;
--- src/home/home.cmake
+++ src/home/home.cmake
@@ -15,12 +15,6 @@ foreach(module ${MODULES})
 	include("${CMAKE_CURRENT_LIST_DIR}/${module}/${module}.cmake")
 endforeach()
 
-execute_process(
-	COMMAND git log -1 --format=%h
-	WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
-	OUTPUT_VARIABLE GIT_HASH
-	OUTPUT_STRIP_TRAILING_WHITESPACE
-	)
 configure_file(${BUILDSCRIPTS_ROOT}/helpers/git_hash.h.in ${CMAKE_CURRENT_BINARY_DIR}/config/git_hash.h @ONLY)
 configure_file(${BUILDSCRIPTS_ROOT}/helpers/version.h.in ${CMAKE_CURRENT_BINARY_DIR}/config/version.h @ONLY)
 
