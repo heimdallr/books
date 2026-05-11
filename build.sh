f778b87b5dc181e5d9527bed6a01c67aaf8aeacb
Alexander Chiryatiev <heimdallrnsk@gmail.com>
2026-05-11 17:51:42 +0700

    Мелкие правки

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ if [ -z "$GENERATOR" ]; then
     GENERATOR=TXZ
 fi
 
-cmake .. -DCMAKE_BUILD_TYPE=Release -DQt6_DIR=/opt/Qt/6.11.0/gcc15.2_Release/lib/cmake/Qt6 -DCPACK_GENERATOR=$GENERATOR -G Ninja
+cmake .. -DCMAKE_BUILD_TYPE=Release -DQt6_DIR=/opt/Qt/6.11.0/gcc16/lib/cmake/Qt6 -DCPACK_GENERATOR=$GENERATOR -G Ninja
 cmake --build . --config Release --parallel
 LD_LIBRARY_PATH=$PWD/lib:$LD_LIBRARY_PATH cpack -C Release
--- src/home/flibrary/version/build.h
+++ src/home/flibrary/version/build.h
@@ -1,1 +1,1 @@
-constexpr int BUILD_NUMBER = 18655;
+constexpr int BUILD_NUMBER = 18693;
