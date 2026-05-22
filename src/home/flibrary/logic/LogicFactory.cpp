a8bbfbd717e830e9b013fd917bd1b8c39f0203d6
Alexander Chiryatev <heimdallrnsk@gmail.com>
2026-05-22 16:20:21 +0700

    #1225 А это настоящее устранение краша

--- src/home/flibrary/logic/LogicFactory.cpp
+++ src/home/flibrary/logic/LogicFactory.cpp
@@ -433,7 +433,10 @@ Util::ExtractedBooks LogicFactory::GetExtractedBooks(QAbstractItemModel* model, 
 			                         .libId     = book[9].toLongLong(),
 			                         .lang      = book[10] };
 
-		for (auto genre = result.genre; !genre.isEmpty(); genre = m_impl->genreParents.at(genre))
+		for (auto genre = result.genre; !genre.isEmpty(); genre = [&] {
+				 const auto it = m_impl->genreParents.find(genre);
+				 return it != m_impl->genreParents.end() ? it->second : QString {};
+			 }())
 			result.genreTree.push_front(genre);
 
 		return result;
