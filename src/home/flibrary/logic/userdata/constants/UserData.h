1302441a87618b807ac0dc399c5e762e37424588
Alexander Chiryatev <heimdallrnsk@gmail.com>
2026-08-20 00:12:56 +0700

    #1347 Обобщены константы

--- src/home/flibrary/logic/userdata/constants/UserData.h
+++ src/home/flibrary/logic/userdata/constants/UserData.h
@@ -2,11 +2,18 @@
 
 #include <functional>
 
+#include "interface/localization.h"
+
 class QString;
 
 namespace HomeCompa::Flibrary::UserData
 {
 
 using Callback = std::function<void(const QString&)>;
+
+inline constexpr auto CONTEXT          = "UserData";
+inline constexpr auto CANNOT_READ_FROM = QT_TRANSLATE_NOOP("UserData", "Cannot read from %1");
+
+TR_DEF
 
 }
